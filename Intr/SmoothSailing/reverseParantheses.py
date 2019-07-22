4eacc8b3a9d05b911fe80f2236edb8e788606b67
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-07-21 20:56:56 -0700

    [idk] really

--- Intr/SmoothSailing/reverseParantheses.py
+++ Intr/SmoothSailing/reverseParantheses.py
@@ -2,19 +2,23 @@ def reverseParentheses(s):
     between = s[s.find("(")+1:s.find(")")]
     return between[::-1]
 
-
-
- 
-# voted #1 solution
-def reverseParenthesesOne(s):
-    for i in range(len(s)):
-        if s[i] == "(":
-            start = i
-        if s[i] == ")":
-            end = i
-            return reverseParentheses(s[:start] + s[start+1:end][::-1] + s[end+1:])
-    return s
-
-
 s = "a(bcdefghijkl(mno)p)q"
-print(reverseParenthesesOne(s))
+s2 = "foo(bar(baz))blim"
+s3 = '(bar)'
+print(reverseParentheses(s2))
+
+def reverse_parantheses(s):
+    """ 
+        Reverses characters in (possibly nested) 
+        parentheses in the input string.
+    """
+    # traverse the string
+    # once "(" found 
+    for i in len(s):
+        if s[i] == '(':
+            start = i
+        if s[i] == ')':
+            end = i
+        
+    return s[end:start]
+        
