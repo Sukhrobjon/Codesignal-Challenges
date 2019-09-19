7594a25cbb0f12b537061e3fc8e278572a427145
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-19 14:45:28 -0700

    [optimized] first duplicate problem

--- Interview Practice/first_duplicate.py
+++ Interview Practice/first_duplicate.py
@@ -1,4 +1,4 @@
-def find_first_dublicate(arr):
+def find_first_duplicate(arr):
     """
     Return the number for which the second occurrence has a smaller index
     than the second occurrence of the other number does. If there are no 
@@ -10,26 +10,25 @@ def find_first_dublicate(arr):
 
     # for each value in the list:
     # i check if I saw the value before if i see the value before
-    # check if value is not none that means we already grabbed the 2nd
+    # check if value is not equal to len(arr) that means we already grabbed the 2nd
     # occurance of the element I add it to the dictionary with the current
     # index if it is the 1st occurance of the elem set the value equal to none
 
-    ind_dict = {}
-    unique = len(arr)
-    for i, elem in enumerate(arr):
-        if elem in ind_dict:
-            if ind_dict[elem] == unique:
-                ind_dict[elem] = i
+    ind_set = set()
+
+    for elem in arr:
+        if elem in ind_set:
+            return elem
         else:
-            ind_dict[elem] = unique
-    # finds the min value and returns the key associated with that value
-    return min(ind_dict, key=ind_dict.get)
+            ind_set.add(elem)
+    # # finds the min value and returns the key associated with that value
+    # return min(ind_dict, key=ind_dict.get)
 """
 a: [2, 1, 3, 5, 3, 2] => 3
 a: [2, 2] => 2
 a: [5, 5, 5, 5, 5] => 5
 
 """
-a = [1, 2, 3, 4, 5]
+a = [2, 1, 3, 5, 3, 2]
 
-print(find_first_dublicate(a))
+print(find_first_duplicate(a))
