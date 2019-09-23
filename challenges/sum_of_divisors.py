e3a8c85efab59b9737d2731ad449e692033ba2e1
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-09-23 12:58:56 -0700

    [documented] sum of divisors

--- challenges/sum_of_divisors.py
+++ challenges/sum_of_divisors.py
@@ -1,33 +1,33 @@
 import math
 
 
-def sum_of_divisors(n):
-    """Find the sum of all divisors of a given integer."""
-    result = 0
-    i = 1
-    sum_l = []
-    while i <= math.sqrt(n):
+def sum_of_divisors_v1(n):
+    """Naive version: Find the sum of all divisors of a given integer."""
+    sum_div = 0
+    for i in range(1, n+1):
+        # check if n is divisible by current number from 1 to n inclusive
         if n % i == 0:
-            result += i
-            sum_l.append(i)
-        else:
-            result += i + (n//i)
-            sum_l.append(i)
-            sum_l.append(n//i)
-        i += 1
-    print(sum_l)
-    return result
+            sum_div += i
+    return sum_div
 
 
 def sum_of_divisors_v2(n):
-    """Find the sum of all divisors of a given integer."""
+    """Find the sum of all divisors of a given integer.
+    NOTE: If we look carefully, all the divisors are present in pairs. For
+    example if n = 100, then the various pairs of divisors are: (1,100),
+    (2,50), (4,25), (5,20), (10,10).
+    resource: https://www.geeksforgeeks.org/find-divisors-natural-number-set-1/
+    """
     result = 0
     i = 1
+    # loop through only sqrt(n) times
     while i <= math.sqrt(n):
+        # we want to find the pair of divisors
         if n % i == 0:
+            # if pairs are equal, add to result only one of them
             if i == (n//i):
                 result += i
-                # print(f"divisor: {i}")
+            # else, add both pairs of divisors to the result
             else:
                 result += i + (n//i)
         i += 1
@@ -36,30 +36,3 @@ def sum_of_divisors_v2(n):
 
 n = 12
 print(sum_of_divisors_v2(n))
-
-
-# method to print the divisors
-def printDivisors(n):
-    # Note that this loop runs till square root
-    i = 1
-    sum_l = []
-    while i <= math.sqrt(n):
-
-        if (n % i == 0):
-
-            # If divisors are equal, print only one
-            if (n / i == i):
-                print(i)
-                sum_l.append(i)
-            else:
-                # Otherwise print both
-                print(i, n//i)
-                sum_l.append(i)
-                sum_l.append(n//i)
-        i += 1
-    return sum_l
-
-
-# Driver method
-# print("The divisors of 100 are: ")
-# print(printDivisors(12))
